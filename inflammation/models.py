d7c643b7bd2571005d8640dd5921441448955b86
Sam Mangham <mangham@gmail.com>
2024-10-08 15:44:28 +0100

    Fix spelling error.

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,12 +19,12 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2d inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array."""
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2d inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array."""
     return np.max(data, axis=0)
 
 
